b1d05641d9bb5ac4adee46ff10243d9cdace0c05
skyfroger <skyfroger@gmail.com>
2018-02-23 18:30:41 +0300

    Partial solution ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // begining of algorithm
+  /*
+  Adaptation of python algorithm
+  http://interactivepython.org/courselib/static/pythonds/BasicDS/BalancedSymbols(AGeneralCase).html
+  */
+  var openers = new Array();
+  var closers = new Array();
+  var stack = new Array();
+
+  for(var i = 0; i < bracketsConfig.length; i++){
+    openers.push(bracketsConfig[i][0]);
+    closers.push(bracketsConfig[i][1]);
+  }
+
+  var balanced = true;
+  var index = 0;
+
+  while(index < str.length && balanced){
+      var symbol = str[index];
+      if(openers.includes(symbol)){
+          stack.push(symbol);
+      } else {
+          if(stack.length == 0)
+              balanced = false;
+          else {
+              var top = stack.pop();
+              var openIndex = openers.indexOf(top);
+              var closeIndex = closers.indexOf(symbol);
+              if (!(openIndex == closeIndex))
+                  balanced = false;
+          }
+      }
+      index++;
+  }
+
+  if(balanced && (stack.length == 0))
+      return true;
+  else
+      return false;
 }
